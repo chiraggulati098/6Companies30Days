b8adaf27cecbebb29b0fc1bc79c236cd9c092c3a
Chirag Gulati <chiraggulati098.com>
2025-01-12 18:35:26 +0530

    did amazon ques 7

---
+++ 02_Amazon/387_First Unique Character in a String.py
@@ -0,0 +1,11 @@
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        freq = defaultdict(int)
+        for c in s:
+            freq[c] += 1
+
+        for i in range(len(s)):
+            if freq[s[i]] == 1:
+                return i
+        
+        return -1
